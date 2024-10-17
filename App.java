bfbd8b47d56d048c210e48734665e0e37876cea6
Inkavvv~ <inka1805@student.hamk.fi>
2024-10-18 01:30:25 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+        String tekija;
+        tekija = "Inka";
+        System.out.println("Ohjelman tekija: " + tekija);
+        
+        int luku1;
+        int luku2;
+        luku1 = 5;
+        luku2 = 10; 
+        System.out.println("Luku1-muuttujan arvo on 5.0");
+        System.out.println("Luku2-muuttujan arvo on 10.0" );
+
+
+        
+
+    }
+}
