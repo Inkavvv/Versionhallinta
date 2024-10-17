25241d19136ae9a4c3bc56f8e4a95dedebab1b1e
Inkavvv~ <inka1805@student.hamk.fi>
2024-10-18 01:35:09 +0300

    Update App.java

--- App.java
+++ App.java
@@ -14,6 +14,7 @@ public class App {
         System.out.println("Luku1 on " + number1);
         System.out.println("Luku2 on " + number2);
         tulo = number1 * number2;
+        //tulostetaan kertolaskun tulos
         System.out.println("Kertolaskun tulos on " + tulo);
     
 
